331e86f21eb7ea6b141cbabf50e1906271ba4122
zeinab <49206934+zeinabss@users.noreply.github.com>
2019-06-02 11:03:43 -0400

    Update karatsuba.py

--- karatsuba.py
+++ karatsuba.py
@@ -1,15 +1,11 @@
-# -*- coding: utf-8 -*-
 """
 Created on Mon Apr  8 14:36:29 2019
 
-@author: pkari
+@author: zeinabss
 
 This function calculates multiplication of two number (positive or negative) using Karatsuba algorithm. 
 """
 import math
-
-x=12145
-y=88
 
 def DigitCount(num):
     if num == 0:
@@ -39,19 +35,24 @@ def karatsuba_multiply(num1,num2):
     firsthalf_num1, secondhalf_num1=split(num1,splitlen1)
     firsthalf_num2, secondhalf_num2=split(num2,splitlen2)
     
+    # first number = ab
+    # second number = cd
+    
     ac=karatsuba_multiply(firsthalf_num1,firsthalf_num2)
     bd=karatsuba_multiply(secondhalf_num1,secondhalf_num2)
     
     if splitlen1==splitlen2:
         ##Gauss's trick
+        # r=(a+b)*(c+d) , t=ad+bc
         r=karatsuba_multiply(firsthalf_num1+secondhalf_num1,firsthalf_num2+secondhalf_num2)
         t=r-bd-ac
         product=ac*10**(splitlen1+splitlen2)+bd+t*10**int(splitlen1)
     else:
+        #future imporovement:
+        #develope the Gauss's trick for this condition! 
         ad=karatsuba_multiply(firsthalf_num1,secondhalf_num2)
         cb=karatsuba_multiply(firsthalf_num2,secondhalf_num1)
         product=ac*10**(splitlen1+splitlen2)+bd+ad*10**int(splitlen1)+cb*10**int(splitlen2)
     return product
 
 
-ans=karatsuba_multiply(x,y)
